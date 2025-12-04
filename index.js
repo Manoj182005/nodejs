a08eea35d34b72a86b11418d6a71f75427c27ecc
Manoj Patel BM <manojpatelbm182005@gmail.com>
2025-12-04 10:13:25 +0530

    nodejs connect with html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const http = require('http');
+// Please don't change the pre-written code
+// Import the necessary modules here
 
-const server = http.createServer((req , res ) =>{
-    res.end('welcome to nodejs server');
-});
+// Write your code here
+const http = require('http')
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    const data = fs.readFileSync('index.html').toString()
+    res.end(data);
 
-server.listen(8080,()=>{
-    console.log('Response received at port 8080.');
-});
+})
+server.listen(8080)
+console.log('server is listening to port 8080');
+
+module.exports = server;
