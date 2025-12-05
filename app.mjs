bf480f42d82c7f5bbc1031c3b3e56be23d254031
Manoj Patel BM <manojpatelbm182005@gmail.com>
2025-12-05 21:38:03 +0530

    ES6 pratice

---
+++ app.mjs
@@ -0,0 +1,11 @@
+// app.js - our main application file
+import {sum} from './math.js';
+import { mean } from './math.js';
+// const math = require("./math.js");
+
+
+const nums = [1, 2, 3, 4, 5];
+
+
+console.log(`The sum is ${sum(nums)}`);
+console.log(`The mean is ${mean(nums)}`);
