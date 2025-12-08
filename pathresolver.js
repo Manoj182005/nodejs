e558117a6521db278cf0ef665343e1cee2d40ed2
Manoj Patel BM <manojpatelbm182005@gmail.com>
2025-12-08 23:33:26 +0530

    FS path

---
+++ pathresolver.js
@@ -0,0 +1,9 @@
+// Import the necessary modules here.
+const path = require('path')
+
+
+
+exports.getAbsolutePath = (filePath) => {
+// Write your code here
+return path.resolve("src","file.txt");
+};
